47e24092e4e3dc5900606df034348c45e5f5f521
Hazri Jordan <hazrijordan@gmail.com>
2018-08-11 07:36:58 +0800

    Ready for Heroku deployment

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import "semantic-ui-css/semantic.css";
 import App from "./App";
 // Removed registerServiceWorker because it clashes it Rails routing and may resolve it unable to access ActiveAdmin.
 
